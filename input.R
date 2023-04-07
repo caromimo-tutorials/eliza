843bce97aa672da2cf2759877f445e781a484e1b
Caroline Mimeault <carolinemimeault@gmail.com>
2023-04-06 21:12:23 -0400

    move REPL into run.R

--- input.R
+++
@@ -1,8 +0,0 @@
-input <- ""
-cat("Eliza: Hello, I am Eliza!\n")
-while (TRUE) {
-  cat("\nYou: ")
-  input <- readLines(file("stdin"), n=1)
-  if (input == "quit") break
-  cat("Eliza:", Eliza(input))
-}
--- run.R
+++ run.R
@@ -1,6 +1,14 @@
-# https://www.r-bloggers.com/2021/01/eliza-chatbot-in-r-build-yourself-a-shrink/
+# source: https://www.r-bloggers.com/2021/01/eliza-chatbot-in-r-build-yourself-a-shrink/
 
 source("model.R")
 source("default_model.R")
 source("eliza.R")
-source("input.R")
+
+input <- ""
+cat("Eliza: Hello, I am Eliza!\n")
+while (TRUE) {
+  cat("\nYou: ")
+  input <- readLines(file("stdin"), n=1)
+  if (input == "quit") break
+  cat("Eliza:", Eliza(input))
+}
